c8db0fd643406b0a555468b79fd04417bc0d05d6
GJ Events <gjeventsoff@gmail.com>
2026-08-08 02:01:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import base44 from "@base44/vite-plugin"Collapse commentComment on line R1gjevents commented on Aug 8, 2026 gjeventson Aug 8, 2026OwnerAuthorMore actionsdReactWrite a replyCode has comments. Press enter to view.
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
@@ -5,6 +6,13 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: "./",
   plugins: [
+    base44({
+      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
+      hmrNotifier: true,
+      navigationNotifier: true,
+      analyticsTracker: true,
+      visualEditAgent: true
+    }),
     react(),
   ],
   build: {
